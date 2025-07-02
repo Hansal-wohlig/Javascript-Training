2b9cf76cf7caac7e94f25d1dac4b80706a5279dc
Hansal <hansal.bhangale@wohlig.com>
2025-07-02 10:48:30 +0530

    Datatypes

---
+++ Datatypes.js
@@ -0,0 +1,48 @@
+// String
+let name = "Alice";
+
+// Number
+let age = 25;
+let pi = 3.14;
+
+// Boolean
+let isStudent = true;
+
+// Undefined
+let job;
+
+// Null
+let car = null;
+
+// Object
+let person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 30
+};
+
+// Array
+let colors = ["red", "green", "blue"];
+
+// Function
+function greet() {
+  return "Hello!";
+}
+
+// Symbol
+let sym = Symbol("id");
+
+// BigInt
+let bigNumber = 1234567890123456789012345678901234567890n;
+
+// Output all values
+console.log("String:", name);
+console.log("Number:", age, pi);
+console.log("Boolean:", isStudent);
+console.log("Undefined:", job);
+console.log("Null:", car);
+console.log("Object:", person);
+console.log("Array:", colors);
+console.log("Function:", greet());
+console.log("Symbol:", sym);
+console.log("BigInt:", bigNumber);
