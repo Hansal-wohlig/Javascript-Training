a158d00720ddff7ae181a758aa4de6ef0076f01f
Hansal <hansal.bhangale@wohlig.com>
2025-07-02 12:01:13 +0530

    Arrow function with arguements

--- Arrow_Functions.js
+++ Arrow_Functions.js
@@ -10,3 +10,13 @@ const person = {
 
 person.regularFunc(); 
 person.arrowFunc();    
+
+//Arguement function taking arguement from parent scope
+
+function withArguments() {
+  const arrow = () => {
+    console.log("Arrow arguments:", arguments); // Inherits from withArguments
+  };
+  arrow(1, 2, 3); // These don't matter
+}
+withArguments(10, 20, 30);
