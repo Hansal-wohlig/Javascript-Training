f59bb34b26923735c8537ab4aa0a48363df37e3e
Hansal <hansal.bhangale@wohlig.com>
2025-07-03 10:29:48 +0530

    callback function and callback hell

--- Callback_Functions.js
+++ Callback_Functions.js
@@ -1,54 +1,23 @@
-console.log("=== Callback Function Demo ===");
-
-function greet(callback) {
-  console.log("Hello!");
-  callback();
+function getData(dataId, getNextData){
+  setTimeout(()=>{
+      console.log("data",dataId)
+      if(getNextData){
+        getNextData()
+      }
+  }, 2000)
 }
 
-function sayGoodbye() {
-  console.log("Goodbye!");
-}
 
-greet(sayGoodbye);
-
-function greetUser(name, callback) {
-  console.log("Hi " + name + "!");
-  callback(name);
-}
-
-function thankUser(user) {
-  console.log("Thanks for visiting, " + user + "!");
-}
-
-greetUser("Alice", thankUser);
-
-function process(callback) {
-  console.log("Processing...");
-  callback();
-}
-
-process(function() {
-  console.log("Anonymous callback done!");
-});
-
-process(() => {
-  console.log("Arrow callback done!");
-});
-
-
-
-function fetchData(callback) {
-  console.log("Fetching data... (simulated)");
-  setTimeout(() => {
-    console.log("Data fetched");
-    callback();
-  }, 1000);
-}
-
-function onFetched() {
-  console.log("Now processing the fetched data...");
-}
-
-fetchData(onFetched);
+//callback hell
+getData(1,()=>{
+  console.log("getting data2")
+  getData(2,()=>{
+    console.log("getting data3")
+    getData(3,()=>{
+      console.log("getting data4")
+      getData(4)
+  })
+})
+})
 
 
