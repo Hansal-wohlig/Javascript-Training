2968d3f69d76e2142d6becc464063c52c49bfdc8
Hansal <hansal.bhangale@wohlig.com>
2025-07-10 12:05:34 +0530

    json basics

---
+++ Express/basic_json.js
@@ -0,0 +1,53 @@
+const express= require("express")
+const app=express()
+const {products} = require("./data.js")
+
+app.get('/',(req,res)=>{
+    res.send("<h1>Home page</h1><a href='/api/products'>products</a>")
+})
+
+app.get('/api/products',(req,res)=>{
+    const newProducts = products.map((product)=>{
+        const{id,name,image} = product
+        return {id,name,image}
+    })
+    res.json(newProducts)
+})
+
+app.get("/api/products/:productId",(req,res)=>{
+    const {productId} = req.params
+    const singleProduct = products.find((product)=>product.id==Number(productId))
+    if(!singleProduct){
+        return res.status(404).send("Product does not exist")
+    }
+    res.json(singleProduct)
+})
+
+
+
+
+app.get("/api/v1/query",(req,res)=>{
+    const {search, limit} = req.query
+    let sortedProducts = [...products]
+    
+    if (search){
+        sortedProducts = sortedProducts.filter((product)=>{
+            return product.name.startsWith(search)
+        })
+    }
+
+    if (limit){
+        sortedProducts = sortedProducts.slice(0,Number(limit))
+    }
+
+    if (sortedProducts.length<1){
+        return res.status(200).send("No product matched your search")
+    }
+    res.status(200).json(sortedProducts)
+})
+
+
+
+app.listen(5000,()=>{
+    console.log("Server is running on port 5000")
+})
